8a933576de0c68747f3fdcd93b6798efa204c905
samue <samuel.doyen@efrei.net>
2025-03-26 14:23:24 +0100

    Page d'accueil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,5 +15,16 @@
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh; /* Prend toute la hauteur de l'écran */
+  width: 100%;
+  background-image: url("@/assets/Ecran_Fond.png"); /* Image de fond */
+  background-size: cover; /* Couvre tout l'écran */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed; /* Empêche le défilement */
+}
 
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,107 @@
 <template>
-    <div>
-      <h1>Bienvenue sur Gamedle</h1>
-      <p>Testez vos connaissances en devinant des jeux de société.</p>
-      <router-link to="/jeu">Jouer</router-link>
+  <div class="game-container">
+    <img src="@/assets/Gamedle_Logo.png" alt="Gamedle" class="logo" />
+
+    <h2>Devine un jeu de société</h2>
+
+    <div class="button-container">
+      <router-link to="/jeu" class="mode-button">Classique</router-link>
+      <p class="hint">Des indices à chaque essai</p>
     </div>
-  </template>
-  
+
+    <router-link to="/a-propos" class="about-button">À propos</router-link>
+  </div>
+</template>
+
+
+<style scoped>
+.game-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%; /* Prend toute la largeur */
+  text-align: center;
+  color: white;
+  position: relative;
+  padding-bottom: 20%; /* Ajout d'espace en bas pour le bouton "À propos" */
+}
+
+
+/* Logo */
+.logo {
+  width: 400px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+/* Titre */
+h2 {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color:black;
+}
+
+/* Conteneur du bouton "Classique" */
+.button-container {
+  background: rgba(255, 255, 255, 0.5);
+  padding: 20px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  height: 100px;    
+}
+
+/* Bouton "Classique" */
+.mode-button {
+  background: rgba(255, 255, 255, 0.7);
+  border: none;
+  padding: 10px 20px;
+  font-size: 18px;
+  cursor: pointer;
+  border-radius: 8px;
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  transition: 0.3s;
+  width: 25%;
+  height: 200px;  
+  display: flex; 
+  align-items: center; 
+  justify-content: center;
+  text-align: center; 
+}
+
+
+.mode-button:hover {
+  background: #ddd;
+}
+
+/* Texte d'indice */
+.hint {
+  font-size: 14px;
+  font-weight: bold;
+  color: black;
+}
+
+/* Bouton "À propos" */
+.about-button {
+  position: absolute;
+  bottom: 20px;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  background: rgba(0, 0, 0, 0.6);
+  padding: 10px 20px;
+  border-radius: 8px;
+  transition: 0.3s;
+}
+
+.about-button:hover {
+  background: rgba(0, 0, 0, 0.8);
+}
+</style>
